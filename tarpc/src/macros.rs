1a0054a00f33feaeff9b35dc4c6942a2d74b566d
Tim Kuehn <tikue@google.com>
2016-02-14 20:33:42 -0800

    Move benchmark into tarpc_examples, allowing tarpc to test on stable

--- tarpc/src/macros.rs
+++ tarpc/src/macros.rs
@@ -525,8 +525,8 @@ mod functional_test {
 
     #[test]
     fn serde() {
-        let _ = env_logger::init();
         use bincode;
+        let _ = env_logger::init();
 
         let request = __Request::add((1, 2));
         let ser = bincode::serde::serialize(&request, bincode::SizeLimit::Infinite).unwrap();
@@ -536,62 +536,5 @@ mod functional_test {
         } else {
             panic!("Expected __Request::add, got {:?}", de);
         }
-    }
-}
-
-#[cfg(test)]
-#[allow(dead_code)] // generated Client isn't used in this benchmark
-mod benchmark {
-    extern crate env_logger;
-    use ServeHandle;
-    use std::sync::{Arc, Mutex};
-    use test::Bencher;
-
-    service! {
-        rpc hello(s: String) -> String;
-    }
-
-    struct HelloServer;
-    impl Service for HelloServer {
-        fn hello(&self, s: String) -> String {
-            format!("Hello, {}!", s)
-        }
-    }
-
-    // Prevents resource exhaustion when benching
-    lazy_static! {
-        static ref HANDLE: Arc<Mutex<ServeHandle>> = {
-            let handle = serve("localhost:0", HelloServer, None).unwrap();
-            Arc::new(Mutex::new(handle))
-        };
-        static ref CLIENT: Arc<Mutex<AsyncClient>> = {
-            let addr = HANDLE.lock().unwrap().local_addr().clone();
-            let client = AsyncClient::new(addr, None).unwrap();
-            Arc::new(Mutex::new(client))
-        };
-    }
-
-    #[bench]
-    fn hello(bencher: &mut Bencher) {
-        let _ = env_logger::init();
-        let client = CLIENT.lock().unwrap();
-        let concurrency = 100;
-        let mut futures = Vec::with_capacity(concurrency);
-        let mut count = 0;
-        bencher.iter(|| {
-            futures.push(client.hello("Bob".into()));
-            count += 1;
-            if count % concurrency == 0 {
-                // We can't block on each rpc call, otherwise we'd be
-                // benchmarking latency instead of throughput. It's also
-                // not ideal to call more than one rpc per iteration, because
-                // it makes the output of the bencher harder to parse (you have
-                // to mentally divide the number by `concurrency` to get
-                // the ns / iter for one rpc
-                for f in futures.drain(..) {
-                    f.get().unwrap();
-                }
-            }
-        });
     }
 }
