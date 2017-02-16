517477129cdee6d9eada1e9c9514cd9b18ba41bf
Tim Kuehn <tikue@google.com>
2017-02-16 01:00:40 -0800

    Remove dead code

--- src/server.rs
+++ src/server.rs
@@ -138,22 +138,3 @@ fn listener(addr: &SocketAddr, handle: &Handle) -> io::Result<TcpListener> {
         .listen(PENDING_CONNECTION_BACKLOG)
         .and_then(|l| TcpListener::from_listener(l, addr, handle))
 }
-
-/// A future that resolves to a `ServerHandle`.
-#[doc(hidden)]
-pub struct ListenFuture {
-    inner: future::FutureResult<io::Result<SocketAddr>, futures::Canceled>,
-}
-
-impl Future for ListenFuture {
-    type Item = SocketAddr;
-    type Error = io::Error;
-
-    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
-        // Can't panic the oneshot is always completed.
-        match self.inner.poll().unwrap() {
-            Async::Ready(result) => result.map(Async::Ready),
-            Async::NotReady => Ok(Async::NotReady),
-        }
-    }
-}
