b9c98162cfd0e66bb9dbb11ed95f626c8aef1438
Tim Kuehn <tikue@google.com>
2018-08-30 18:56:41 -0700

    Fix example

--- examples/server_calling_server.rs
+++ examples/server_calling_server.rs
@@ -3,7 +3,13 @@
 // Licensed under the MIT License, <LICENSE or http://opensource.org/licenses/MIT>.
 // This file may not be copied, modified, or distributed except according to those terms.
 
-#![feature(plugin, futures_api, await_macro, async_await, existential_type)]
+#![feature(
+    plugin,
+    futures_api,
+    await_macro,
+    async_await,
+    existential_type
+)]
 #![plugin(tarpc_plugins)]
 
 #[macro_use]
@@ -11,13 +17,14 @@ extern crate tarpc;
 #[macro_use]
 extern crate futures;
 
-use crate::add::{Service as AddService};
-use crate::double::{Service as DoubleService};
-use futures::{prelude::*,
-    future::{self, Ready},
-};
+use crate::add::Service as AddService;
+use crate::double::Service as DoubleService;
 use futures::compat::TokioDefaultSpawner;
-use rpc::{client, server};
+use futures::{
+    future::{self, Ready},
+    prelude::*,
+};
+use rpc::{client, server::{self, Handler}};
 use std::io;
 
 pub mod add {
@@ -47,57 +54,61 @@ impl AddService for AddServer {
 
 #[derive(Clone)]
 struct DoubleServer {
-    client: add::Client,
+    add_client: add::Client,
 }
 
-impl DoubleServer {
-    fn new(client: add::Client) -> Self {
-        DoubleServer { client: client }
-    }
-}
-
-existential type Double: Future<Output = Result<i32, String>> + Send;
 
 impl DoubleService for DoubleServer {
-    type DoubleFut = Double;
+    existential type DoubleFut: Future<Output = Result<i32, String>> + Send;
 
     fn double(&self, _: &server::Context, x: i32) -> Self::DoubleFut {
-        let client = self.client.clone();
-        async move {
+        async fn double(mut client: add::Client, x: i32) -> Result<i32, String> {
             let result = await!(client.add(client::Context::current(), x, x));
-             result.map_err(|e| e.to_string())
+            result.map_err(|e| e.to_string())
         }
+
+        double(self.add_client.clone(), x)
     }
 }
 
 async fn run() -> io::Result<()> {
-    let transport = bincode_transport::listen(&"0.0.0.0:0".parse().unwrap())?;
-    let addr = transport.local_addr();
+    let add_listener = bincode_transport::listen(&"0.0.0.0:0".parse().unwrap())?;
+    let addr = add_listener.local_addr();
     let add_server = rpc::Server::new(server::Config::default())
-        .incoming(transport)
+        .incoming(add_listener)
+        .take(1)
         .respond_with(add::serve(AddServer));
     spawn!(add_server);
 
-    let transport = await!(bincode_transport::connect(&addr))?;
-    let mut add_client = await!(add::newStub(client::Config::default(), transport));
+    let to_add_server = await!(bincode_transport::connect(&addr))?;
+    let add_client = await!(add::newStub(client::Config::default(), to_add_server));
 
-    let transport = bincode_transport::listen(&"0.0.0.0:0".parse().unwrap())?;
-    let addr = transport.local_addr();
+    let double_listener = bincode_transport::listen(&"0.0.0.0:0".parse().unwrap())?;
+    let addr = double_listener.local_addr();
     let double_server = rpc::Server::new(server::Config::default())
-        .incoming(transport)
-        .respond_with(double::serve(DoubleServer{client: add_client}));
+        .incoming(double_listener)
+        .take(1)
+        .respond_with(double::serve(DoubleServer { add_client }));
     spawn!(double_server);
 
-    let transport = await!(bincode_transport::connect(&addr))?;
-    let mut double_client = await!(double::newStub(client::Config::default(), transport));
+    let to_double_server = await!(bincode_transport::connect(&addr))?;
+    let mut double_client = await!(double::newStub(client::Config::default(), to_double_server));
 
-    for i in 0i32..5 {
-        println!("{:?}", await!(double_client.double(client::Context::current(), i))?);
+    for i in 1..=5 {
+        println!(
+            "{:?}",
+            await!(double_client.double(client::Context::current(), i))?
+        );
     }
     Ok(())
 }
 
 fn main() {
     env_logger::init();
-    tokio::run(run().map_err(|e| panic!(e)).boxed().compat(TokioDefaultSpawner));
+    tokio::run(
+        run()
+            .map_err(|e| panic!(e))
+            .boxed()
+            .compat(TokioDefaultSpawner),
+    );
 }
