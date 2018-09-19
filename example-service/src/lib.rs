baa70fab48e2258afeaa4959977b5cd872eb8735
Tim Kuehn <tikue@google.com>
2018-09-19 02:10:34 -0700

    (WIP) split example into lib and main files

--- example-service/src/lib.rs
+++ example-service/src/lib.rs
@@ -1,6 +1,16 @@
-#![feature(plugin, futures_api, pin, arbitrary_self_types, await_macro, async_await, existential_type)]
+#![feature(
+    plugin,
+    futures_api,
+    pin,
+    arbitrary_self_types,
+    await_macro,
+    async_await,
+    existential_type
+)]
 #![plugin(tarpc_plugins)]
 
+// This is the service definition. It looks a lot like a trait definition.
+// It defines one RPC, hello, which takes one arg, name, and returns a String.
 tarpc::service! {
     /// Returns a greeting for name.
     rpc hello(name: String) -> String;
