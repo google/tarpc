950ad5187c1c91c027127f3ee8462da26758dccf
Artem Vorotnikov <artem@vorotnikov.me>
2019-05-21 04:45:41 +0300

    Add JSON transport (#219)

---
+++ json-transport/src/lib.rs
@@ -0,0 +1,203 @@
+// Copyright 2019 Google LLC
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+//! A TCP [`Transport`] that serializes as JSON.
+
+#![feature(arbitrary_self_types, async_await)]
+#![deny(missing_docs)]
+
+use futures::{compat::*, prelude::*, ready};
+use pin_utils::unsafe_pinned;
+use serde::{Deserialize, Serialize};
+use std::{
+    error::Error,
+    io,
+    marker::PhantomData,
+    net::SocketAddr,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tokio::codec::{length_delimited::LengthDelimitedCodec, Framed};
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_serde_json::*;
+use tokio_tcp::{TcpListener, TcpStream};
+
+/// A transport that serializes to, and deserializes from, a [`TcpStream`].
+pub struct Transport<S: AsyncWrite, Item, SinkItem> {
+    inner: Compat01As03Sink<
+        ReadJson<WriteJson<Framed<S, LengthDelimitedCodec>, SinkItem>, Item>,
+        SinkItem,
+    >,
+}
+
+impl<S: AsyncWrite, Item, SinkItem> Transport<S, Item, SinkItem> {
+    unsafe_pinned!(
+        inner:
+            Compat01As03Sink<
+                ReadJson<WriteJson<Framed<S, LengthDelimitedCodec>, SinkItem>, Item>,
+                SinkItem,
+            >
+    );
+}
+
+impl<S, Item, SinkItem> Stream for Transport<S, Item, SinkItem>
+where
+    S: AsyncWrite + AsyncRead,
+    Item: for<'a> Deserialize<'a>,
+{
+    type Item = io::Result<Item>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<io::Result<Item>>> {
+        match self.inner().poll_next(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(None) => Poll::Ready(None),
+            Poll::Ready(Some(Ok(next))) => Poll::Ready(Some(Ok(next))),
+            Poll::Ready(Some(Err(e))) => {
+                Poll::Ready(Some(Err(io::Error::new(io::ErrorKind::Other, e))))
+            }
+        }
+    }
+}
+
+impl<S, Item, SinkItem> Sink<SinkItem> for Transport<S, Item, SinkItem>
+where
+    S: AsyncWrite,
+    SinkItem: Serialize,
+{
+    type SinkError = io::Error;
+
+    fn start_send(self: Pin<&mut Self>, item: SinkItem) -> io::Result<()> {
+        self.inner()
+            .start_send(item)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        convert(self.inner().poll_ready(cx))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        convert(self.inner().poll_flush(cx))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        convert(self.inner().poll_close(cx))
+    }
+}
+
+fn convert<E: Into<Box<Error + Send + Sync>>>(poll: Poll<Result<(), E>>) -> Poll<io::Result<()>> {
+    match poll {
+        Poll::Pending => Poll::Pending,
+        Poll::Ready(Ok(())) => Poll::Ready(Ok(())),
+        Poll::Ready(Err(e)) => Poll::Ready(Err(io::Error::new(io::ErrorKind::Other, e))),
+    }
+}
+
+impl<Item, SinkItem> rpc::Transport for Transport<TcpStream, Item, SinkItem>
+where
+    Item: for<'de> Deserialize<'de>,
+    SinkItem: Serialize,
+{
+    type Item = Item;
+    type SinkItem = SinkItem;
+
+    fn peer_addr(&self) -> io::Result<SocketAddr> {
+        self.inner
+            .get_ref()
+            .get_ref()
+            .get_ref()
+            .get_ref()
+            .peer_addr()
+    }
+
+    fn local_addr(&self) -> io::Result<SocketAddr> {
+        self.inner
+            .get_ref()
+            .get_ref()
+            .get_ref()
+            .get_ref()
+            .local_addr()
+    }
+}
+
+/// Returns a new JSON transport that reads from and writes to `io`.
+pub fn new<Item, SinkItem>(io: TcpStream) -> Transport<TcpStream, Item, SinkItem>
+where
+    Item: for<'de> Deserialize<'de>,
+    SinkItem: Serialize,
+{
+    Transport::from(io)
+}
+
+impl<S: AsyncWrite + AsyncRead, Item: serde::de::DeserializeOwned, SinkItem: Serialize> From<S>
+    for Transport<S, Item, SinkItem>
+{
+    fn from(inner: S) -> Self {
+        Transport {
+            inner: Compat01As03Sink::new(ReadJson::new(WriteJson::new(Framed::new(
+                inner,
+                LengthDelimitedCodec::new(),
+            )))),
+        }
+    }
+}
+
+/// Connects to `addr`, wrapping the connection in a JSON transport.
+pub async fn connect<Item, SinkItem>(
+    addr: &SocketAddr,
+) -> io::Result<Transport<TcpStream, Item, SinkItem>>
+where
+    Item: for<'de> Deserialize<'de>,
+    SinkItem: Serialize,
+{
+    Ok(new(TcpStream::connect(addr).compat().await?))
+}
+
+/// Listens on `addr`, wrapping accepted connections in JSON transports.
+pub fn listen<Item, SinkItem>(addr: &SocketAddr) -> io::Result<Incoming<Item, SinkItem>>
+where
+    Item: for<'de> Deserialize<'de>,
+    SinkItem: Serialize,
+{
+    let listener = TcpListener::bind(addr)?;
+    let local_addr = listener.local_addr()?;
+    let incoming = listener.incoming().compat();
+    Ok(Incoming {
+        incoming,
+        local_addr,
+        ghost: PhantomData,
+    })
+}
+
+/// A [`TcpListener`] that wraps connections in JSON transports.
+#[derive(Debug)]
+pub struct Incoming<Item, SinkItem> {
+    incoming: Compat01As03<tokio_tcp::Incoming>,
+    local_addr: SocketAddr,
+    ghost: PhantomData<(Item, SinkItem)>,
+}
+
+impl<Item, SinkItem> Incoming<Item, SinkItem> {
+    unsafe_pinned!(incoming: Compat01As03<tokio_tcp::Incoming>);
+
+    /// Returns the address being listened on.
+    pub fn local_addr(&self) -> SocketAddr {
+        self.local_addr
+    }
+}
+
+impl<Item, SinkItem> Stream for Incoming<Item, SinkItem>
+where
+    Item: for<'a> Deserialize<'a>,
+    SinkItem: Serialize,
+{
+    type Item = io::Result<Transport<TcpStream, Item, SinkItem>>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let next = ready!(self.incoming().poll_next(cx)?);
+        Poll::Ready(next.map(|conn| Ok(new(conn))))
+    }
+}
---
+++ json-transport/tests/bench.rs
@@ -0,0 +1,101 @@
+// Copyright 2019 Google LLC
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+//! Tests client/server control flow.
+
+#![feature(test, integer_atomics, async_await)]
+
+use futures::{compat::Executor01CompatExt, prelude::*};
+use libtest::stats::Stats;
+use rpc::{
+    client, context,
+    server::{Handler, Server},
+};
+use std::{
+    io,
+    time::{Duration, Instant},
+};
+
+async fn bench() -> io::Result<()> {
+    let listener = tarpc_json_transport::listen(&"0.0.0.0:0".parse().unwrap())?;
+    let addr = listener.local_addr();
+
+    tokio_executor::spawn(
+        Server::<u32, u32>::default()
+            .incoming(listener)
+            .take(1)
+            .respond_with(|_ctx, request| futures::future::ready(Ok(request)))
+            .unit_error()
+            .boxed()
+            .compat(),
+    );
+
+    let conn = tarpc_json_transport::connect(&addr).await?;
+    let client = &mut client::new::<u32, u32, _>(client::Config::default(), conn).await?;
+
+    let total = 10_000usize;
+    let mut successful = 0u32;
+    let mut unsuccessful = 0u32;
+    let mut durations = vec![];
+    for _ in 1..=total {
+        let now = Instant::now();
+        let response = client.call(context::current(), 0u32).await;
+        let elapsed = now.elapsed();
+
+        match response {
+            Ok(_) => successful += 1,
+            Err(_) => unsuccessful += 1,
+        };
+        durations.push(elapsed);
+    }
+
+    let durations_nanos = durations
+        .iter()
+        .map(|duration| duration.as_secs() as f64 * 1E9 + duration.subsec_nanos() as f64)
+        .collect::<Vec<_>>();
+
+    let (lower, median, upper) = durations_nanos.quartiles();
+
+    println!("Of {} runs:", durations_nanos.len());
+    println!("\tSuccessful: {}", successful);
+    println!("\tUnsuccessful: {}", unsuccessful);
+    println!(
+        "\tMean: {:?}",
+        Duration::from_nanos(durations_nanos.mean() as u64)
+    );
+    println!("\tMedian: {:?}", Duration::from_nanos(median as u64));
+    println!(
+        "\tStd Dev: {:?}",
+        Duration::from_nanos(durations_nanos.std_dev() as u64)
+    );
+    println!(
+        "\tMin: {:?}",
+        Duration::from_nanos(durations_nanos.min() as u64)
+    );
+    println!(
+        "\tMax: {:?}",
+        Duration::from_nanos(durations_nanos.max() as u64)
+    );
+    println!(
+        "\tQuartiles: ({:?}, {:?}, {:?})",
+        Duration::from_nanos(lower as u64),
+        Duration::from_nanos(median as u64),
+        Duration::from_nanos(upper as u64)
+    );
+
+    Ok(())
+}
+
+#[test]
+fn bench_small_packet() -> io::Result<()> {
+    env_logger::init();
+    rpc::init(tokio::executor::DefaultExecutor::current().compat());
+
+    tokio::run(bench().map_err(|e| panic!(e.to_string())).boxed().compat());
+    println!("done");
+
+    Ok(())
+}
