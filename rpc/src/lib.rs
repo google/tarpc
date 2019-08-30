541401579b9033ec22bf91daa577111e90b028b3
Artem Vorotnikov <artem@vorotnikov.me>
2019-08-30 16:20:30 +0300

    tokio 0.2.0-alpha.4

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -4,7 +4,7 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-#![feature(weak_counts, non_exhaustive, async_await, trait_alias)]
+#![feature(weak_counts, non_exhaustive, trait_alias)]
 #![deny(missing_docs, missing_debug_implementations)]
 
 //! An RPC framework providing client and server.
