31c713d188369676618488444a01ebe21fb8e703
Oleg Nosov <olegnosov1@gmail.com>
2019-12-12 21:13:57 +0300

    Allow raw identifiers + fixed naming + place all code generation methods in impl (#291)
    Allows defining services using raw identifiers like:
    
    ```rust
    pub mod service {
        #[tarpc::service]
        pub trait r#trait {
            async fn r#fn(x: i32) -> Result<u8, String>;
        }
    }
    ```
    
    Also:
    
    - Refactored names (ident -> type)
    - All code generation methods placed in impl

--- plugins/tests/service.rs
+++ plugins/tests/service.rs
@@ -29,6 +29,38 @@ fn att_service_trait() {
     }
 }
 
+#[allow(non_camel_case_types)]
+#[test]
+fn raw_idents() {
+    use futures::future::{ready, Ready};
+
+    type r#yield = String;
+
+    #[tarpc::service]
+    trait r#trait {
+        async fn r#await(r#struct: r#yield, r#enum: i32) -> (r#yield, i32);
+        async fn r#fn(r#impl: r#yield) -> r#yield;
+        async fn r#async();
+    }
+
+    impl r#trait for () {
+        type AwaitFut = Ready<(r#yield, i32)>;
+        fn r#await(self, _: context::Context, r#struct: r#yield, r#enum: i32) -> Self::AwaitFut {
+            ready((r#struct, r#enum))
+        }
+
+        type FnFut = Ready<r#yield>;
+        fn r#fn(self, _: context::Context, r#impl: r#yield) -> Self::FnFut {
+            ready(r#impl)
+        }
+
+        type AsyncFut = Ready<()>;
+        fn r#async(self, _: context::Context) -> Self::AsyncFut {
+            ready(())
+        }
+    }
+}
+
 #[test]
 fn syntax() {
     #[tarpc::service]
