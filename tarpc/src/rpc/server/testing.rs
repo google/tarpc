54d6e0e3b67e780b47b0347e1b74930d13176d78
Tim Kuehn <tikue@google.com>
2020-08-04 17:33:41 -0700

    Add license headers

--- tarpc/src/rpc/server/testing.rs
+++ tarpc/src/rpc/server/testing.rs
@@ -1,3 +1,9 @@
+// Copyright 2020 Google LLC
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 use crate::server::{Channel, Config};
 use crate::{context, Request, Response};
 use fnv::FnvHashSet;
