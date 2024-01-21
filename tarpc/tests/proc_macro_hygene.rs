40d2d163b61a014514b6770d72107d1a1a713355
OMGeeky <39029799+OMGeeky@users.noreply.github.com>
2024-01-21 11:51:18 -0800

    procedural macro hygiene (#417)
    see https://doc.rust-lang.org/reference/procedural-macros.html#procedural-macro-hygiene
    
    * cleanup std usages
    * Fully qualify Some, Ok and Err when inside quoted area
    * Fix renaming of import
    * Add test file that creates a compiler error when hygiene is violated/stuff is used from outer scope

---
+++ tarpc/tests/proc_macro_hygene.rs
@@ -0,0 +1,17 @@
+#![no_implicit_prelude]
+extern crate tarpc as some_random_other_name;
+
+#[cfg(feature = "serde1")]
+mod serde1_feature {
+    #[::tarpc::derive_serde]
+    #[derive(Debug, PartialEq, Eq)]
+    pub enum TestData {
+        Black,
+        White,
+    }
+}
+
+#[::tarpc::service]
+pub trait ColorProtocol {
+    async fn get_opposite_color(color: u8) -> u8;
+}
