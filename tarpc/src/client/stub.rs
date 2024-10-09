eebdd450fd3f8f27d615a5bf49d1a1dd8573b789
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2024-10-09 20:56:28 +0800

    Implement an easier way to get Send Serve and Stub

--- tarpc/src/client/stub.rs
+++ tarpc/src/client/stub.rs
@@ -1,9 +1,11 @@
 //! Provides a Stub trait, implemented by types that can call remote services.
 
+use std::future::Future;
+
 use crate::{
     client::{Channel, RpcError},
     context,
-    server::Serve,
+    server::{SendServe, Serve},
     RequestName,
 };
 
@@ -15,7 +17,6 @@ mod mock;
 
 /// A connection to a remote service.
 /// Calls the service with requests of type `Req` and receives responses of type `Resp`.
-#[allow(async_fn_in_trait)]
 pub trait Stub {
     /// The service request type.
     type Req: RequestName;
@@ -24,8 +25,28 @@ pub trait Stub {
     type Resp;
 
     /// Calls a remote service.
-    async fn call(&self, ctx: context::Context, request: Self::Req)
-        -> Result<Self::Resp, RpcError>;
+    fn call(
+        &self,
+        ctx: context::Context,
+        request: Self::Req,
+    ) -> impl Future<Output = Result<Self::Resp, RpcError>>;
+}
+
+/// A connection to a remote service.
+/// Calls the service with requests of type `Req` and receives responses of type `Resp`.
+pub trait SendStub: Send {
+    /// The service request type.
+    type Req: RequestName;
+
+    /// The service response type.
+    type Resp;
+
+    /// Calls a remote service.
+    fn call(
+        &self,
+        ctx: context::Context,
+        request: Self::Req,
+    ) -> impl Future<Output = Result<Self::Resp, RpcError>> + Send;
 }
 
 impl<Req, Resp> Stub for Channel<Req, Resp>
@@ -40,9 +61,35 @@ where
     }
 }
 
+impl<Req, Resp> SendStub for Channel<Req, Resp>
+where
+    Req: RequestName + Send,
+    Resp: Send,
+{
+    type Req = Req;
+    type Resp = Resp;
+
+    async fn call(&self, ctx: context::Context, request: Req) -> Result<Self::Resp, RpcError> {
+        Self::call(self, ctx, request).await
+    }
+}
+
 impl<S> Stub for S
 where
     S: Serve + Clone,
+{
+    type Req = S::Req;
+    type Resp = S::Resp;
+    async fn call(&self, ctx: context::Context, req: Self::Req) -> Result<Self::Resp, RpcError> {
+        self.clone().serve(ctx, req).await.map_err(RpcError::Server)
+    }
+}
+
+impl<S> SendStub for S
+where
+    S: SendServe + Clone + Sync,
+    S::Req: Send + Sync,
+    S::Resp: Send,
 {
     type Req = S::Req;
     type Resp = S::Resp;
