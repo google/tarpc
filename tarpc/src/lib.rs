bacf2c76692515fd67c8118e6a590e5a02fe67bc
Tim Kuehn <tikue@google.com>
2024-10-26 18:38:33 -0700

    Prepare release of tarpc v0.35.0

--- tarpc/src/lib.rs
+++ tarpc/src/lib.rs
@@ -54,7 +54,7 @@
 //! Add to your `Cargo.toml` dependencies:
 //!
 //! ```toml
-//! tarpc = "0.34"
+//! tarpc = "0.35"
 //! ```
 //!
 //! The `tarpc::service` attribute expands to a collection of items that form an rpc service.
@@ -69,7 +69,7 @@
 //! ```toml
 //! anyhow = "1.0"
 //! futures = "0.3"
-//! tarpc = { version = "0.34", features = ["tokio1"] }
+//! tarpc = { version = "0.35", features = ["tokio1"] }
 //! tokio = { version = "1.0", features = ["macros"] }
 //! ```
 //!
