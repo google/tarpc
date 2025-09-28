8701b5da787a655d60de4a1a87cad0ee4fabaaed
Tim Kuehn <tikue@google.com>
2025-09-28 13:59:20 -0700

    Update bincode to version 2.0

--- tarpc/examples/compression.rs
+++ tarpc/examples/compression.rs
@@ -74,7 +74,7 @@ where
 }
 
 fn serialize<T: Serialize>(t: T) -> io::Result<ByteBuf> {
-    bincode::serialize(&t)
+    bincode::serde::encode_to_vec(&t, bincode::config::standard())
         .map(ByteBuf::from)
         .map_err(io::Error::other)
 }
@@ -83,7 +83,9 @@ fn deserialize<D>(message: ByteBuf) -> io::Result<D>
 where
     for<'a> D: Deserialize<'a>,
 {
-    bincode::deserialize(message.as_ref()).map_err(io::Error::other)
+    let (d, _) = bincode::serde::decode_from_slice(message.as_ref(), bincode::config::standard())
+        .map_err(io::Error::other)?;
+    Ok(d)
 }
 
 fn add_compression<In, Out>(
